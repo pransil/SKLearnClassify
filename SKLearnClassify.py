58008e262e293ac47df99ec1b0bc08f6e5237064
Pat Ransil <pat.ransil@gmail.com>
2018-11-28 13:40:43 -0800

    Initial commit

---
+++ SKLearnClassify.py
@@ -0,0 +1,24 @@
+# Simple SKLearn code for decision tree classsifier
+
+from sklearn import datasets
+iris =  datasets.load_iris()
+
+X = iris.data
+y = iris.target
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3)
+
+#from sklearn.neighbors import KNeighborsClassifier
+#my_classifier = KNeighborsClassifier()
+
+from sklearn import tree
+my_classifier = tree.DecisionTreeClassifier()
+
+my_classifier.fit(X_train, y_train)
+
+predictions = my_classifier.predict(X_test)
+
+from sklearn.metrics import accuracy_score
+#from sklearn.metrics import accuracy_score
+print (accuracy_score(y_test, predictions))
